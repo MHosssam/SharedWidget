bf5775daf1f21054391628d05330184c850b6fca
mohamed hossam <mhosssam000@gmail.com>
2021-07-11 13:42:23 +0200

    NoItemWidget

---
+++ lib/no_item_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shared_widget/text_widget.dart';
+
+class NoItemWidget extends StatelessWidget {
+  final Widget child;
+  final String text;
+  final double? height;
+
+  const NoItemWidget({
+    Key? key,
+    required this.child,
+    required this.text,
+    this.height,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        height: height ?? 250,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: child,
+            ),
+            TextWidget(
+              text,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
