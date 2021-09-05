f54a0991ea058c6ff09415e75c7db9fc2c72de61
mohamed hossam <mhosssam000@gmail.com>
2021-09-05 16:12:57 +0200

    Dio Helper

---
+++ lib/doi_helper.dart
@@ -0,0 +1,162 @@
+import 'package:dio/dio.dart';
+
+///TODO:
+/*
+ * dio: ^4.0.0 
+ */
+
+class DioHelper {
+  static Dio? dio;
+
+  static init() {
+    dio = Dio(
+      BaseOptions(
+        ///TODO: Base Url.
+        baseUrl: 'https://student.valuxapps.com/api/',
+        receiveDataWhenStatusError: true,
+      ),
+    );
+  }
+
+  static Future<Response> getData({
+    required String url,
+    Map<String, dynamic>? query,
+    String lang = 'en',
+    String? token,
+  }) async {
+    dio!.options.headers = {
+      'Content-Type': 'application/json',
+      'lang': lang,
+      'Authorization': token ?? '',
+    };
+
+    return await dio!.get(
+      url,
+      queryParameters: query ?? null,
+    );
+  }
+
+  static Future<Response> postData({
+    required String url,
+    Map<String, dynamic>? query,
+    required Map<String, dynamic> data,
+    String lang = 'en',
+    String? token,
+  }) async {
+    dio!.options.headers = {
+      'Content-Type': 'application/json',
+      'lang': lang,
+      'Authorization': token ?? '',
+    };
+
+    return dio!.post(
+      url,
+      queryParameters: query,
+      data: data,
+    );
+  }
+}
+
+///TODO:
+/*
+
+*Ex get method :
+
+void getHomeData()
+  {
+
+    DioHelper.getData(
+      url: HOME,
+      token: token,
+    ).then((value)
+    {
+      homeModel = HomeModel.fromJson(value.data);
+
+      printFullText(homeModel.data.banners[0].image);
+      print(homeModel.status);
+
+    }).catchError((error)
+    {
+      print(error.toString());
+    });
+  }
+}
+
+*Ex post method :
+
+void userLogin({
+    @required String email,
+    @required String password,
+  })
+  {
+
+    DioHelper.postData(
+      url: LOGIN,
+      data:
+      {
+        'email': email,
+        'password': password,
+      },
+    ).then((value)
+    {
+      print(value.data);
+      loginModel = ShopLoginModel.fromJson(value.data);
+    }).catchError((error)
+    {
+      print(error.toString());
+    });
+  }
+
+  *Ex model :
+
+  class ShopLoginModel
+{
+  bool status;
+  String message;
+  UserData data;
+
+  ShopLoginModel.fromJson(Map<String, dynamic> json)
+  {
+    status = json['status'];
+    message = json['message'];
+    data = json['data'] != null ? UserData.fromJson(json['data']) : null;
+  }
+}
+
+class UserData
+{
+  int id;
+  String name;
+  String email;
+  String phone;
+  String image;
+  int points;
+  int credit;
+  String token;
+
+  UserData({
+    this.id,
+    this.name,
+    this.email,
+    this.phone,
+    this.image,
+    this.points,
+    this.credit,
+    this.token,
+  });
+
+  // named constructor
+  UserData.fromJson(Map<String, dynamic> json)
+  {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+    image = json['image'];
+    points = json['points'];
+    credit = json['credit'];
+    token = json['token'];
+  }
+}
+
+ */
