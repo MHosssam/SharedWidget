5693abc251b81602b107d1b124fc4f20176267ec
mohamed hossam <mhosssam000@gmail.com>
2021-12-27 16:24:31 +0200

    edit

--- lib/app_form_builder_text_field.dart
+++ lib/app_form_builder_text_field.dart
@@ -1,65 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-class AppFormBuilderTextField extends StatelessWidget {
+class AppFormBuilderTextField extends StatefulWidget {
   const AppFormBuilderTextField({
     Key? key,
     required this.name,
     this.labelText,
-    required this.validator,
+    this.valueTransformer,
+    this.validator,
     this.hintText,
-    this.maxLines,
-    this.width,
-    this.obscureText,
+    this.obscureText = false,
     this.keyboardType,
     this.labelColor,
+    this.maxLines = 1,
     this.cursorColor,
-    this.borderCOlor,
     this.suffixIcon,
+    this.textDirection,
     this.prefixIcon,
+    this.onChanged,
     this.enabled = true,
+    this.isCollapsed = false,
   }) : super(key: key);
 
-  final String name;
+  final bool enabled, obscureText;
+  final String? labelText;
   final int? maxLines;
-  final double? width;
-  final bool? enabled, obscureText;
-  final String? hintText, labelText;
-  final Widget? suffixIcon, prefixIcon;
+  final String name;
+  final String? hintText;
   final TextInputType? keyboardType;
-  final Color? labelColor, borderCOlor, cursorColor;
+  final Color? labelColor, cursorColor;
+  final TextDirection? textDirection;
+  final Widget? suffixIcon, prefixIcon;
+  final void Function(String?)? onChanged;
   final String? Function(String?)? validator;
+  final bool isCollapsed;
+  final dynamic Function(String? value)? valueTransformer;
+  @override
+  State<AppFormBuilderTextField> createState() =>
+      _AppFormBuilderTextFieldState();
+}
 
+class _AppFormBuilderTextFieldState extends State<AppFormBuilderTextField> {
+  late bool isSecure = widget.obscureText;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 45,
-      width: width ?? 200,
+    return Card(
+      elevation: 4,
       child: FormBuilderTextField(
-        name: name,
-        maxLines: maxLines ?? 1,
-        keyboardType: keyboardType,
-        validator: validator,
-        obscureText: obscureText ?? false,
-        cursorColor: cursorColor ?? Colors.black,
+        name: widget.name,
+        obscureText: isSecure,
+        onChanged: (value) => widget.onChanged,
+        valueTransformer: widget.valueTransformer,
+        keyboardType: widget.keyboardType,
+        textDirection: widget.textDirection,
+        maxLines: widget.maxLines,
+        cursorColor: widget.cursorColor ?? Colors.black,
         decoration: InputDecoration(
-          labelText: labelText,
-          hintText: hintText,
-          floatingLabelStyle: TextStyle(color: labelColor ?? Colors.black),
-          enabled: enabled!,
+          isCollapsed: widget.isCollapsed,
+          labelText: widget.labelText,
+          hintText: widget.hintText,
+          floatingLabelStyle: TextStyle(
+            color: widget.labelColor ?? Colors.black,
+          ),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8.0),
           ),
-          focusColor: Colors.black,
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(
-              color: borderCOlor ?? Colors.black,
+            borderSide: const BorderSide(
+              color: Colors.transparent,
             ),
           ),
-          suffixIcon: suffixIcon,
-          prefixIcon: prefixIcon,
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+          ),
+          enabled: widget.enabled,
+          focusColor: Colors.black,
+          suffixIcon: widget.suffixIcon ??
+              (widget.obscureText
+                  ? IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isSecure = !isSecure;
+                        });
+                      },
+                      icon: Icon(
+                        isSecure ? Icons.visibility_off : Icons.remove_red_eye,
+                      ),
+                    )
+                  : null),
+          prefixIcon: widget.prefixIcon,
         ),
+        validator: widget.validator,
       ),
     );
   }
